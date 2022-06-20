4e040fd1a87cb6bd1e608e87bc0b0089e495a170
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-06-20 17:45:00 +0300

    fixed some elements

--- node_modules/telegram/CryptoFile.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const platform_1 = require("./platform");
-const crypto = require(platform_1.isNode ? "crypto" : "./crypto/crypto");
-exports.default = crypto;
